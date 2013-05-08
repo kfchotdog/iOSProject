a07bac51b71a43ba082f726723da44b35efc4804
JackCheng <kfchotdog@hotmail.com>
2013-05-08 18:18:13 +0800

    山寨保卫萝卜工程创建

---
+++ CarrotShanZhai/CarrotShanZhai/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  CarrotShanZhai
+//
+//  Created by 程显坤 on 13-5-8.
+//  Copyright CarrotShanZhai 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ CarrotShanZhai/CarrotShanZhai/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  CarrotShanZhai
+//
+//  Created by 程显坤 on 13-5-8.
+//  Copyright CarrotShanZhai 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
